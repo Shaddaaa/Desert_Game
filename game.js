b95fe69226f49334186259068f733110ab6eb2c7
Shaddaaa <florian@kaufmann.cloud>
2019-05-21 19:19:20 +0200

    added different colors for story and informational text

--- game.js
+++ game.js
@@ -3,20 +3,20 @@ class Game {
 		this.inventory = new Inventory();
 		this.logElement = document.getElementById("log");
 		this.resourceElement = document.getElementById("resources");
-		this.log("You awake. Around you is nothing but desert. There's a hill with a little cavern.", false);
-		this.log("You see some small animals prints. Maybe you can survive out here.", false);
+		this.log("You awake. Around you is nothing but desert. There's a hill with a little cavern.", true);
+		this.log("You see some small animals prints. Maybe you can survive out here.", true);
 	}
 
-	log(message, showAmount = true) {
+	log(message, story = false) {
 		//if the same message is sent multiple times, append the count instead of adding a new message
-		if(showAmount && log.lastChild != null && log.lastChild.innerHTML.substring(0,message.length)==message) {
+		if(!story && log.lastChild != null && log.lastChild.innerHTML.substring(0,message.length)==message) {
 			//get the number behind the last x representing the amount of times this message has been sent in a row
 			let count = log.lastChild.innerHTML.substring(log.lastChild.innerHTML.lastIndexOf("x") + 1, log.lastChild.innerHTML.lastIndexOf("<"));
 			//instead of adding a new message, the amount at the end is adjusted
 			log.lastChild.innerHTML = message + " x" + (parseInt(count)+1) + "<br>";
 		} else {
 			//creates a new message in the log
-			this.logElement.innerHTML += "<div class=logMsg>" + message + (showAmount ? " x1" : "") + "<br></div>";
+			this.logElement.innerHTML += "<div class='logMsg'" + (story ? "" : "style='color: gray'") + ">" + message + (story ? "" : " x1") + "<br></div>";
 		}
 		//scrolls to the bottom of the console
 		this.logElement.scrollTop = this.logElement.scrollHeight;
