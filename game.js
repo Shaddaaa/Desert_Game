3217db788966c514cbbdd2950b0ac64567c9d297
Shaddaaa <florian@kaufmann.cloud>
2019-05-21 17:43:26 +0200

    replaced some old code with the lastChild() function

--- game.js
+++ game.js
@@ -3,25 +3,21 @@ class Game {
 		this.inventory = new Inventory();
 		this.logElement = document.getElementById("log");
 		this.resourceElement = document.getElementById("resources");
+		this.resourceCollectionElement = document.getElementById("");
 		this.log("You awake. Around you is nothing but desert. There's a hill with a little cavern.", false);
 		this.log("You see some small animals prints. Maybe you can survive out here.", false);
 	}
 
 	log(message, showAmount = true) {
 		//if the same message is sent multiple times, append the count instead of adding a new message
-		if(showAmount && document.getElementById("lastMsg") != null && document.getElementById("lastMsg").innerHTML.substring(0,message.length)==message) {
-			let lastMsg = document.getElementById("lastMsg").innerHTML;
+		if(showAmount && log.lastChild != null && log.lastChild.innerHTML.substring(0,message.length)==message) {
 			//get the number behind the last x representing the amount of times this message has been sent in a row
-			let count = lastMsg.substring(lastMsg.lastIndexOf("x") + 1, lastMsg.lastIndexOf("<"));
+			let count = log.lastChild.substring(lastMsg.lastIndexOf("x") + 1, log.lastChild.lastIndexOf("<"));
 			//instead of adding a new message, the amount at the end is adjusted
-			document.getElementById("lastMsg").innerHTML = message + " x" + (parseInt(count)+1) + "<br>";
+			log.lastChild.innerHTML = message + " x" + (parseInt(count)+1) + "<br>";
 		} else {
-			//removes the id="lastMsg" from the last message
-			let tmp = this.logElement.innerHTML.split('id="lastMsg"');
-			if(tmp != "")
-				this.logElement.innerHTML = tmp[0] + tmp[1];
-			//creates a new logMsg div with the id="lastMsg"
-			this.logElement.innerHTML += "<div class=logMsg id='lastMsg'>" + message + (showAmount ? " x1" : "") + "<br></div>";
+			//creates a new message in the log
+			this.logElement.innerHTML += "<div class=logMsg>" + message + (showAmount ? " x1" : "") + "<br></div>";
 		}
 		//scrolls to the bottom of the console
 		this.logElement.scrollTop = this.logElement.scrollHeight;
@@ -39,6 +35,7 @@ class Game {
 	digSand() {
 		this.inventory.add("sand", 1);
 		this.log("You found 1 sand!");
+		callEvent("digSand");
 	}
 
 	digDirt() {
