4a8659d8614ce8dba4b17ff3ff657b0f7b9e07b1
Shaddaaa <florian@kaufmann.cloud>
2019-05-22 17:47:00 +0200

    extended the inventory class by a remove(resource, amount) function also made the add & remove functions dispatch an event each

--- inventory.js
+++ inventory.js
@@ -4,18 +4,46 @@ class Inventory {
 		this.resourceAmount = [];
 	}
 	add(resource, amount) {
-		if (amount < 0) {
+		if(amount < 0) {
 			game.log("This is an error, please report it to the developer. Code:addnegativ");
+			return;
 		}
-		for (let i = 0; i < this.resources.length; i++) {
+		for(let i = 0; i < this.resources.length; i++) {
 			if(this.resources[i] == resource) {
 				this.resourceAmount[i] += amount;
+				eventManager.invoke("addedResource");
 				game.updateResourceDisplay();
 				return;
 			}
 		}
 		this.resources.push(resource);
 		this.resourceAmount.push(amount);
+		eventManager.invoke("addedResource");
 		game.updateResourceDisplay();
+	}
+	remove(resource, amount) {
+		if(amount < 0) {
+			game.log("This is an error, please report it to the developer. Code:removenegativ");
+			return false;
+		}
+		let index = this.getResourceIndex(resource);
+		if(index!=null) {
+			if(amount > this.resourceAmount[index]) {
+				game.log("Can't afford that yet!");
+				return false;
+			}
+			this.resourceAmount[index] -= amount;
+			eventManager.invoke("removedResource");
+			return true;
+		}
+		return false;
+	}
+	getResourceIndex(resource) {
+		for(let i = 0; i < this.resources.length; i++) {
+			if(this.resources[i] == resource) {
+				return i;
+			}
+		}
+		return null;
 	}
 }
